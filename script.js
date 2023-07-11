349e0a09460a7741697ffd31e491dfdcacc63a05
sharif721 <smuyanja11@gmail.com>
2023-07-10 19:31:04 -0700

    new

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // PUSH TO FRONT
 
-// var item = 7, i, arr = [9,9];
-// PushFront(arr);
-// function PushFront(arr) {
-//     for (i = arr.length-1; i >= 0; --i) {
-//         arr[i + 1] = arr[i];
-//     }
-//     arr[0] = item;
-// };
-// console.log(arr);
+var item = 7, i, arr = [9, 9];
+PushFront(arr);
+function PushFront(arr) {
+    for (i = arr.length - 1; i >= 0; --i) {
+        arr[i + 1] = arr[i];
+    }
+    arr[0] = item;
+};
+console.log(arr);
 
 
 // POP FRONT
 
-// Array.prototype.removeItem = function (item) {
-//     for (i = 0; i < this.length; i++) {
-//         if (this[i] === item) {
-//             for (var j = i; j < this.length - 1; j++) {
-//                 this[j] = this[j + 1];
-//             }
-//             this.length = this.length - 1
-//             return;
-//         }
-//     }
-// }
+Array.prototype.removeItem = function (item) {
+    for (i = 0; i < this.length; i++) {
+        if (this[i] === item) {
+            for (var j = i; j < this.length - 1; j++) {
+                this[j] = this[j + 1];
+            }
+            this.length = this.length - 1
+            return;
+        }
+    }
+}
 
-// var items = ['a', 'b', 'c', 'd'];
-// items.removeItem('d');
-// console.log(items);
+var items = ['a', 'b', 'c', 'd'];
+items.removeItem('d');
+console.log(items);
 
 
 
